852c2f91cfa4d4e55981c83f996e178177d4ad46
williamspwhite <williamspwhite@gmail.com>
2020-11-14 15:43:30 -0500

    Initial commit

---
+++ src/GlobalNet.java
@@ -0,0 +1,14 @@
+public class GlobalNet 
+{
+    //creates a global network 
+    //O : the original graph
+    //regions: the regional graphs
+    public static Graph run(Graph O, Graph[] regions) 
+    {
+        return null;
+	    //To be implemented
+    }
+}
+    
+    
+    
---
+++ src/RegNet.java
@@ -0,0 +1,11 @@
+public class RegNet 
+{
+    //creates a regional network
+    //G: the original graph
+    //max: the budget
+    public static Graph run(Graph G, int max) 
+    {
+        return null;
+	    //To be implemented
+    }
+}
