e8d7caf706ade5ff6c176bb94cb8123b7a137720
williamspwhite <williamspwhite@gmail.com>
2020-11-14 16:25:01 -0500

    started RegNet

--- src/RegNet.java
+++ src/RegNet.java
@@ -1,10 +1,21 @@
-public class RegNet 
+import java.util.ArrayList;
+
+public class RegNet
 {
     //creates a regional network
     //G: the original graph
     //max: the budget
     public static Graph run(Graph G, int max) 
     {
+        Graph tempGraph = G.connGraph();
+        ArrayList<Edge> sortedEdges = tempGraph.sortedEdges();
+
+        UnionFind union = new UnionFind(tempGraph.V());
+
+        int
+
+
+
         return null;
 	    //To be implemented
     }
