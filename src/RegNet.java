e79e2ab264a63557d831a335b4d56795574dc0ee
williamspwhite <williamspwhite@gmail.com>
2020-11-18 23:20:17 -0500

    finished sorted edge list

--- src/RegNet.java
+++ src/RegNet.java
@@ -1,3 +1,4 @@
+import javax.print.DocFlavor;
 import java.util.ArrayList;
 
 public class RegNet
@@ -33,41 +34,64 @@ public class RegNet
 
         // Step 2
         int num_possible_Edges = (int) (.5 * (mstGraph.V() - 1) * mstGraph.V());
-        int[] numStopsArray = new int[num_possible_Edges];
-        Edge[] numStopsEdgeArray = new Edge[num_possible_Edges];
-
+        EdgeAndStops[] edgeAndStops_arr = new EdgeAndStops[num_possible_Edges];
+        int edgeAndStops_index = 0;
 
         int[] edgeTo = BFS(mstGraph, sortedEdges.get(0).u); // gets edge for BFS and does BFS
         for (int start_index = 0; start_index < (mstGraph.V() - 1); start_index++) {
             for (int current_index = start_index; current_index < mstGraph.V(); current_index++) {
-
+                edgeAndStops_arr[edgeAndStops_index] = new EdgeAndStops(mstGraph.getEdge(start_index, current_index),
+                        findStops(edgeTo, start_index, current_index));
+                edgeAndStops_index++;
             }
         }
+
+        edgeAndStops_arr = edgeStop_QuickSort(edgeAndStops_arr, 0, edgeAndStops_arr.length - 1);
+
+
+
+
+
+
+
 
 
 
         return null;
     }
 
-    private static int findStops(int[] edgeTo, int start_index, int end_index) {
-        int numStops = 0;
-        int current_index = start_index;
-        while (current_index != end_index) {
-            if (current_index == -1) {
-                current_index = end_index;
-                while (current_index != -1) { //if stuck at beginning of BFS, search other direction
-                    current_index = edgeTo[current_index];
-                    numStops++;
-                }
-                return numStops;
+
+    /*
+     * findMST()
+     * finds an MST on Graph G using Kruskal's algorithm
+     */
+    private static Graph findMST(Graph G) {
+        Graph tempGraph = G.connGraph();
+        ArrayList<Edge> sortedEdges = tempGraph.sortedEdges();
+        Graph mstGraph = new Graph(tempGraph.V());
+
+        UnionFind union = new UnionFind(tempGraph.V());
+
+        int numTempGraphEdges = tempGraph.E();
+
+        int sortedEdges_index = 0;
+
+        while (mstGraph.E() < (numTempGraphEdges - 1)) {
+            Edge current_edge = sortedEdges.get(sortedEdges_index);
+            if (union.find(current_edge.ui()) != union.find(current_edge.vi())) {
+                mstGraph.addEdge(current_edge.ui(), current_edge.vi(), current_edge.w);
+                union.union(current_edge.ui(), current_edge.vi());
             }
-            current_index = edgeTo[current_index];
-            numStops++;
+            sortedEdges_index++;
         }
-        return numStops;
+
+        return mstGraph;
     }
 
-
+    /*
+     * BFS()
+     * runs BFS on Graph G given a starting vertice
+     */
     private static int[] BFS(Graph G, String start_code) {
         if ((G == null) || (start_code == null)) {
             return null;
@@ -90,26 +114,101 @@ public class RegNet
         }
         return edgeTo;
     }
-    private static Graph findMST(Graph G) {
-        Graph tempGraph = G.connGraph();
-        ArrayList<Edge> sortedEdges = tempGraph.sortedEdges();
-        Graph mstGraph = new Graph(tempGraph.V());
 
-        UnionFind union = new UnionFind(tempGraph.V());
-
-        int numTempGraphEdges = tempGraph.E();
-
-        int sortedEdges_index = 0;
-
-        while (mstGraph.E() < (numTempGraphEdges - 1)) {
-            Edge current_edge = sortedEdges.get(sortedEdges_index);
-            if (union.find(current_edge.ui()) != union.find(current_edge.vi())) {
-                mstGraph.addEdge(current_edge.ui(), current_edge.vi(), current_edge.w);
-                union.union(current_edge.ui(), current_edge.vi());
-            }
-            sortedEdges_index++;
+    /*
+     * finds number of stops between 2 vertices given the edgeTo result
+     * from a BFS
+     */
+    private static int findStops(int[] edgeTo, int start_index, int end_index) {
+        int numStops = -1;
+        int current_index;
+        if (edgeTo[start_index] == -1) {
+            current_index = start_index;
+        } else {
+            current_index = end_index;
         }
 
-        return mstGraph;
+        while (current_index != end_index) {
+            if (current_index == -1) {
+                current_index = end_index;
+                while (current_index != -1) { //if stuck at beginning of BFS, search other direction
+                    current_index = edgeTo[current_index];
+                    numStops++;
+                }
+                return numStops - 2; //subtract 2 bc "-1" vertex counted twice
+            }
+            current_index = edgeTo[current_index];
+            numStops++;
+        }
+        return numStops;
+    }
+
+
+    //TODO
+    //possible problems with sorting
+    /*
+     * edgeStop_QuickSort
+     * sorts an array of edges and stops
+     */
+    private static EdgeAndStops[] edgeStop_QuickSort(EdgeAndStops[] edgeStop_arr, int low, int high) {
+        if ((low < 0) || (high < 0) || (high >= edgeStop_arr.length) || (low >= high)) {
+            System.out.println("BIG FAIL IN QUICKSORT");
+            return edgeStop_arr;
+        }
+
+        int pivot = high;
+        int lessThan = low;
+
+        for (int i = low; i < high; i++) {
+            if (edgeStop_arr[i].compare(edgeStop_arr[pivot]) <= 0) {
+
+                //swap i and pivot
+                EdgeAndStops temp = edgeStop_arr[i];
+                edgeStop_arr[i] = edgeStop_arr[pivot];
+                edgeStop_arr[pivot] = temp;
+
+                lessThan++;
+            }
+        }
+
+        //swap lessThan and pivot
+        EdgeAndStops temp = edgeStop_arr[lessThan];
+        edgeStop_arr[lessThan] = edgeStop_arr[pivot];
+        edgeStop_arr[pivot] = temp;
+
+        edgeStop_arr = edgeStop_QuickSort(edgeStop_arr, low, lessThan - 1);
+        edgeStop_arr = edgeStop_QuickSort(edgeStop_arr, lessThan + 1, high);
+        return edgeStop_arr;
+    }
+}
+
+class EdgeAndStops {
+    private Edge e;
+    private int numStops;
+    public EdgeAndStops(Edge e, int numStops) {
+        this.e = e;
+        this.numStops = numStops;
+    }
+
+    public int getNumStops() { return numStops; }
+    public Edge getEdge() { return e; }
+
+
+    /*
+     * compare()
+     * compares to EdgeAndStops using NumStops
+     * and uses if NumStops are equal
+     */
+    public int compare(EdgeAndStops es) {
+        if (this.getNumStops() == es.getNumStops()) {
+            return es.getEdge().w - this.getEdge().w; //reverse because smaller edge
+                                            // distances are more important
+        } else {
+            return this.getNumStops() - es.getNumStops();
+        }
+    }
+
+    public String toString() {
+        return "" + this.numStops + ", " + this.e.toString();
     }
 }
